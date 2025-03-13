70db6e1b8e9b52521bbffd91aee42387bc8498a3
YuSuhyun <dbtngus99@gmail.com>
2025-03-13 11:22:22 +0900

    테스트 화면 추가가

--- 02/app.py
+++ 02/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app) 
+# CORS(app) 
 
 @app.route("/")
 def index():
@@ -11,8 +12,14 @@ def index():
 def chat():
     return render_template("chat.html")
 
+@app.route("/faceRotation")
+def faceRotation():
+    return render_template("rotation.html")
 
-
+@app.route("/trigger")
+def trigger():
+    return render_template("trigger.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5017, debug=True)
+    app.run( debug=True)
+    # app.run(host="0.0.0.0", port=5017, debug=True)
