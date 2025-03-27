f987cfc3606575e61fa1d0beaea988406283612b
YuSuhyun <dbtngus99@gmail.com>
2025-03-27 10:50:06 +0900

    부분수정정

--- 03/static/js/chat.js
+++ 03/static/js/chat.js
@@ -335,7 +335,7 @@ function clearCanvas() {
 
 
 //////// 채팅 출력 확인 
-
+chat_history = []
 // 전송 
 function summit_chat(){
     let message = $('#text_input').val().replace(/\n/g, ' ');
@@ -343,27 +343,35 @@ function summit_chat(){
     $('#chat_talks').scrollTop($('#chat_talks')[0].scrollHeight);
     $('#text_input').val('');
     $.ajax({
-        url:'/chat/ask',
+        url:'/chat/talks',
         type:'POST',
         contentType: 'application/json',
-        data: JSON.stringify({ message: message }),
+        data: JSON.stringify({
+            message: message,
+            chat_history: chat_history
+         }),
             success: function (response) {
+                chat_history.push({"role": "user", "content": message})
                 console.log(response);
                 let result_type = response.result_type;
                 if (result_type == 'camera'){
                     $('#chat_talks').append(`<div class="ai_talk"><div>${response.text_message}</div></div>`);
                     clearCanvas();
                     setupCamera();
+                    chat_history.push({"role": "assistant", "content": response.text_message})
                 } else if(result_type == 'cheese'){
                     $('#chat_talks').append(`<div class="ai_talk"><div>${response.text_message}</div></div>`);
                     clearCanvas();
                     rotationFace();
+                    chat_history.push({"role": "assistant", "content": response.text_message})
                 }else if(result_type == 'stop'){
                     $('#chat_talks').append(`<div class="ai_talk"><div>${response.text_message}</div></div>`);
                     stopCamera();
                     clearCanvas();
+                    chat_history.push({"role": "assistant", "content": response.text_message})
                 }else if(result_type == 'facefit'){
                     let fit_chat ='';
+                    let save_text ='';
                     response.text_message.forEach(function(shape, idx) {
                         console.log(shape);
                         fit_chat += `
@@ -372,15 +380,18 @@ function summit_chat(){
                                 <img src="../static/img/${shape.img}" alt="${shape.title}" style="width: 150px; object-fit: contain;" />
                                 <p>${shape.desc}</p>
                             </div>`;
+                        save_text += ' ',shape.title, ' : '
+                        save_text += shape.desc
                     });
                     $('#chat_talks').append(`<div class="ai_talk"><div>${fit_chat}</div></div>`);
-                    
-                }else if(result_type == 'message'){
+                    chat_history.push({"role": "assistant", "content": save_text})
+                }else {
                     $('#chat_talks').append(`<div class="ai_talk"><div>${response.text_message}</div></div>`);
-                    
+                    chat_history.push({"role": "assistant", "content": response.text_message})
                 }
                 $('#chat_talks').scrollTop($('#chat_talks')[0].scrollHeight);
-
+                
+                
             },
             error: function (xhr, status, error) {
                 console.log("에러 발생: " + error);
--- 03/views/chat.py
+++ 03/views/chat.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, request, jsonify, render_template
+from langchain_openai import ChatOpenAI
+from langchain.agents import initialize_agent, Tool, AgentExecutor, create_openai_functions_agent
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
 
 chat_bp = Blueprint('chat', __name__, url_prefix='/chat')
 
@@ -35,6 +40,136 @@ face_shapes = [
 def chat_page():
     return render_template('chat.html')
 
+
+# llm 
+def camera_on(input=None):
+    result_type = 'camera'
+    text_message = '사진 촬영을 위해 카메라를 시작합니다. 사진을 찍을 준비가되면 사진촬영이라 말씀해주세요.' 
+    print('camera_on 실행 *****')
+
+    return {"result_type": result_type, 'text_message': text_message}
+
+
+def capture_start(input=None):
+    result_type = 'cheese'
+    text_message = '사진을 촬영합니다, 화면의 중앙에 얼굴을 맞추고 정면을 바라봐 주세요.' 
+    print('capture_start 실행 *****')
+    return {"result_type": result_type, 'text_message': text_message}
+
+
+def camera_stop(input=None):
+    result_type = 'stop'
+    text_message = '사진 촬영을 중단합니다.' 
+    print('camera_stop 실행 *****')
+
+    return {"result_type": result_type, 'text_message': text_message}
+
+
+def face_shape_info(input=None):
+    result_type = 'facefit'
+    text_message = face_shapes
+    print('face_shape_info 실행 *****')
+
+    return {"result_type": result_type, 'text_message': text_message}
+
+
+
+# tool 등록 
+tools = [
+    Tool(
+        name="camera_on",
+        func=camera_on,
+        description="카메라 사용을 시작함"
+    ),
+    Tool(
+        name="capture_start",
+        func=capture_start,
+        description="카운트다운후 사진을 찍음"
+    ),
+    Tool(
+        name="camera_stop",
+        func=camera_stop,
+        description="카메라 사용을 중단함"
+    ),
+    Tool(
+        name="face_shape_info",
+        func=face_shape_info,
+        description="열굴형 타입별 정보를 안내함"
+    ),
+]
+
+#답변 처리 
+
+llm = ChatOpenAI(model='gpt-3.5-turbo')
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessage(content="너는 얼굴형에 따른 안경추천 챗봇이야. 사용자의 요청에 맞게 적절한 기능을 호출해. 적절한 기능이 없는경우 명확한 요청사항을 물어보고, 사용자가 전혀 다른 주제를 이야기 할경우 주의를 줘."),
+    MessagesPlaceholder(variable_name="chat_history"),
+    MessagesPlaceholder(variable_name="input"),
+    MessagesPlaceholder(variable_name="agent_scratchpad")
+])
+agent = create_openai_functions_agent(llm=llm, tools=tools, prompt=prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    handle_parsing_errors=True,
+    return_intermediate_steps=True  
+)
+
+@chat_bp.route('/talks', methods=['POST'])
+def talks():
+
+    user_message = request.json.get("message")
+    chat_history_raw = request.json.get("chat_history", [])
+    print(f'사용자 문구 : {user_message}')
+
+    chat_history = []
+    for msg in chat_history_raw:
+        if msg["role"] == "user":
+            chat_history.append(HumanMessage(content=msg["content"]))
+        elif msg["role"] == "assistant":
+            chat_history.append(AIMessage(content=msg["content"]))
+
+    results = agent_executor.invoke(
+        {   
+            "input": [HumanMessage(content=user_message)],
+            "chat_history": chat_history
+        },
+        config={"return_intermediate_steps": True} 
+    )
+
+    print(f'result : {results}')
+    print(f'results["intermediate_steps"] : {results["intermediate_steps"]}')
+    print(f'len(results["intermediate_steps"]) : {len(results["intermediate_steps"])}')
+
+    if len(results["intermediate_steps"]) != 0:
+        last_step = results["intermediate_steps"][-1]  # tool 의 return 값
+        print(f'last_step*********************** : {last_step}')
+        
+        tool_result = last_step[1]
+        print(f'tool_result*********************** : {tool_result}')
+
+        result = {
+            "result_type": tool_result.get("result_type", "message"),
+            "text_message": tool_result.get("text_message", results["output"])
+        }
+
+    else:
+        result ={
+            "result_type": "message",
+            "text_message": results["output"]
+        }
+    
+
+    return jsonify(result)
+    # return jsonify(response)  
+
+
+
+
+
+
+
+# 이전 사용 
 @chat_bp.route('/ask', methods=['POST'])
 def chat_ask():
     user_message = request.json.get("message")
